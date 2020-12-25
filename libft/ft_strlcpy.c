066121575dad865d5edac3068bc2495e98ac0bfa
Taewan Kim <taewakim@c1r5s5.42seoul.kr>
2020-12-25 16:59:26 +0900

    update

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: taewakim <taewakim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/23 02:05:15 by taewakim          #+#    #+#             */
-/*   Updated: 2020/12/25 01:55:04 by taewakim         ###   ########.fr       */
+/*   Updated: 2020/12/25 14:36:24 by taewakim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 		*dest++ = *src++;
 		count++;
 	}
-	if (count != 0)
+	if (size != 0)
 		*dest = 0;
 	while (*src++)
 		count++;
