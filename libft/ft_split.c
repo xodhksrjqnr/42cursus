5dded668c3fedb63066b8a0baaada9876e488991
Taewan Kim <taewakim@c1r5s5.42seoul.kr>
2020-12-27 02:57:12 +0900

    update split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -1,60 +1,65 @@
 #include "libft.h"
 #include <stdlib.h>
 
-static char		*new_arr(char *start, char *end)
+static char		*new_arr(char const *s, char c)
 {
 	char	*result;
-	char	*tmp;
 	int		len;
 
-	len = end - start;
+	len = 0;
+	while (*(s + len) != c && *(s + len))
+		len++;
 	if (!(result = (char *)malloc(len + 1)))
 		return (0);
-	tmp = result;
-	while (start != end)
-		*tmp++ = *start++;
-	*tmp = 0;
+	result[len] = 0;
+	ft_memcpy(result, s, len);
 	return (result);
 }
 
-static void		make_result(char **result, char *s, int c)
+static int		next_location(char const *s, char c)
 {
-	char	*start;
-	int		count;
+	int		len;
 
-	count = 0;
-	start = s;
+	len = 0;
+	while (*(s + len) != c && *(s + len))
+		len++;
+	return (len);
+}
+
+static int		check_len(char const *s, char c)
+{
+	int		len;
+
+	len = 0;
 	while (*s)
 	{
-		if (*s == c)
-		{
-			*(result + count++) = new_arr(start, s);
-			start = s + 1;
-		}
-		s++;
+		len++;
+		while (*s != c)
+			s++;
+		while (*s == c)
+			s++;
 	}
-	*(result + count++) = new_arr(start, s);
-	*(result + count) = 0;
+	return (len);
 }
 
 char			**ft_split(char const *s, char c)
 {
-	char	*convert_s;
 	char	**result;
-	char	*tmp;
 	int		len;
-	
+	int		count;
+	int		location;
+
 	if (!s)
 		return (0);
-	convert_s = ft_strtrim(s, &c);
-	tmp = convert_s;
-	len = 0;
-	while (*tmp)
-		if (*tmp++ != c)
-			len++;
+	len = check_len(s, c);
 	if (!(result = (char **)malloc(len + 1 * sizeof(char *))))
 		return (0);
-	make_result(result, convert_s, c);
-	free(convert_s);
+	count = 0;
+	location = 0;
+	while (count < len)
+	{
+		*(result + count++) = new_arr(s + location, c);
+		location = next_location(s + location, c);
+	}
 	return (result);
 }
