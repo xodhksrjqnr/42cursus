4cd7b0955358f43f8d98d54c82a4a1e6a82ac910
Taewan Kim <taewakim@c1r5s5.42seoul.kr>
2020-12-26 20:21:50 +0900

    update strtrim

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -1,23 +1,35 @@
 #include "libft.h"
 #include <stdlib.h>
 
+char	*new_arr(char *start, char *end)
+{
+	char	*result;
+	char	*tmp;
+	int		len;
+
+	len = end - start + 1;
+	if (!(result = (char *)malloc(len + 1)))
+		return (0);
+	tmp = result;
+	while (start != end + 1)
+		*tmp++ = *start++;
+	*tmp = 0;
+	return (result);
+}
+
 char	**ft_split(char const *s, char c)
 {
-	char	*cur;
-	char	*tmp;
+	char	*convert_s;
 	char	**result;
 	int		count;
 
+	convert_s = ft_strtrim(s, &c);
+	tmp = convert_s;
 	count = 0;
-	tmp = (char *)s;
 	while (*tmp)
 		if (*tmp++ == c)
 			count++;
-	if (!(result = (char **)malloc(count + 1 * sizeof(char *))))
+	if (!(result = (char **)malloc(count + 2 * sizeof(char *))))
 		return (0);
-	tmp = (char *)s;
-	while (cur = ft_strchr(tmp, c))
-	{
-
-	}
+	result[count + 1] = 0;
 }
