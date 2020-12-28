d53a57272762e77a9fd22ada49e90c97f9648561
Taewan Kim <taewakim@c1r5s5.42seoul.kr>
2020-12-29 00:50:02 +0900

    update strlcat

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,34 +6,27 @@
 /*   By: taewakim <taewakim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/23 02:05:04 by taewakim          #+#    #+#             */
-/*   Updated: 2020/12/27 02:25:06 by taewakim         ###   ########.fr       */
+/*   Updated: 2020/12/29 00:28:43 by taewakim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
+#include "libft.h"
 
 size_t		ft_strlcat(char *dest, const char *src, size_t size)
 {
-	size_t		dest_count;
-	size_t		src_count;
-	size_t		result;
+	size_t		d_count;
+	size_t		s_count;
+	size_t		count;
 
-	dest_count = 0;
-	while (*dest)
-	{
-		dest++;
-		dest_count++;
-	}
-	src_count = 0;
-	while (src_count + dest_count + 1 < size && *src)
-	{
+	d_count = ft_strlen(dest);
+	s_count = ft_strlen(src);
+	if (d_count >= size)
+		return (size + s_count);
+	count = 0;
+	dest += d_count;
+	while (count++ + d_count + 1 < size && *src)
 		*dest++ = *src++;
-		src_count++;
-	}
 	*dest = 0;
-	while (*src++)
-		src_count++;
-	result = (dest_count >= size) ? size : dest_count;
-	result += src_count;
-	return (result);
+	return (s_count + d_count);
 }
