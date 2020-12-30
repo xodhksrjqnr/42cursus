e041f398526bea1ad6f2e6121a72885f6f4c5e20
xodhksrjqnr <xodhksrjqnr@naver.com>
2020-12-30 15:02:17 +0900

    tmp

---
+++ tmp/ft_strncmp.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taewakim <taewakim@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/23 02:05:37 by taewakim          #+#    #+#             */
+/*   Updated: 2020/12/29 20:28:02 by taewakim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t			count;
+
+	if (n == 0 || (s1 == s2))
+		return (0);
+	count = 0;
+	while ((*s1 || *s2) && count++ < n)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	return (0);
+}
