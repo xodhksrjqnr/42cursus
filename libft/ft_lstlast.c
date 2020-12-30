2d868c6bc157bd3b53c5a0b40a846ee265a2e263
Taewan Kim <taewakim@c2r3s7.42seoul.kr>
2020-12-30 23:45:08 +0900

    new

--- libft/ft_lstlast.c
+++ libft/ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: taewakim <taewakim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/27 16:17:32 by taewakim          #+#    #+#             */
-/*   Updated: 2020/12/29 07:36:28 by taewakim         ###   ########.fr       */
+/*   Updated: 2020/12/30 23:40:53 by taewakim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 t_list		*ft_lstlast(t_list *lst)
 {
+	if (!lst)
+		return (NULL);
 	while (lst->next)
 		lst = lst->next;
 	return (lst);
