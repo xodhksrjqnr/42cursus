dc629908fb1036d117a4fd6000fbcb74fc5cf925
Taewan Kim <taewakim@c1r9s2.42seoul.kr>
2021-01-18 16:00:32 +0900

    delete

--- ft_printf/err_check.c
+++
@@ -1,9 +0,0 @@
-#include "ft_printf.h"
-
-int		err_check(const char *s)
-{
-	if (*s == 'c' || *s == 's' || *s == 'p' || *s == 'd' || *s == 'i'
-			|| *s == 'u' || *s == 'x' || *s == 'X' || *s == '%')
-		return (1);
-	return (0);
-}
--- ft_printf/print_adr.c
+++
@@ -1,6 +0,0 @@
-#include "ft_printf.h"
-
-int		print_adr(t_list *list, va_list ap, char *tmp)
-{
-	
-}
