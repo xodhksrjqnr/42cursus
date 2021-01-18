56e91411728de9948ff79e43aafe0711760b4f79
Taewan Kim <taewakim@c1r9s2.42seoul.kr>
2021-01-18 20:48:26 +0900

    up print_c

---
+++ ft_printf/print_num.c
@@ -0,0 +1,6 @@
+#include "ft_printf.h"
+
+void	print_num(t_flags cur, int n, int *count, char *tmp)
+{
+	
+}
--- ft_printf/print_s.c
+++ ft_printf/print_s.c
@@ -1,6 +1,43 @@
 #include "ft_printf.h"
 
+static void		check_dot(t_flags cur, char *s)
+{
+	if (cur.dot)
+	{
+		while (*(s + cur.second))
+		{
+			*(s + cur.second) = 0;
+			s++;
+		}
+	}
+}
+
 void	print_s(t_flags cur, char *s, int *count, char *tmp)
 {
-	
+	int		len;
+	char	*save;
+
+	check_dot(cur, s);
+	len = ft_strlen(s);
+	if (len >= cur.first)
+	{
+		while (*s)
+		{
+			write(1, s++, 1);
+			*count += 1;
+		}
+		break ;
+	}
+	save = tmp;
+	if (cur.minus)
+		while (*s)
+			*tmp++ = *s++;
+	else
+		while (*s)
+			*(tmp++ + cur.first - len) = *s++;
+	while (*save)
+	{
+		write(1, save++, 1);
+		*count += 1;
+	}
 }
