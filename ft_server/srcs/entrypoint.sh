d9a40214690b6984cd5dc4f02e46e0114a948217
user <xodhksrjqnr@naver.com>
2021-02-18 18:52:42 +0900

    second

--- ft_server/srcs/entrypoint.sh
+++ ft_server/srcs/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ "$AUTOINDEX_USE" -eq 1 ]; then
-    sed -i '22s/off/on/' etc/nginx/sites-available/default && \
-    sed -i '16s/^/#/' etc/nginx/sites-available/default
+    sed -i '21s/off/on/' etc/nginx/sites-available/default && \
+    sed -i '15s/^/#/' etc/nginx/sites-available/default
 fi
 service nginx start
 service mysql start
