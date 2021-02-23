611e7edcd115193f3b221ee3e4eb88ff0b679c49
Taewan Kim <taewakim@c4r10s6.42seoul.kr>
2021-02-23 15:59:23 +0900

    up

--- ft_server/srcs/entrypoint.sh
+++ ft_server/srcs/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ "$AUTOINDEX_USE" -eq 1 ]; then
-    sed -i '21s/off/on/' etc/nginx/sites-available/default && \
-    sed -i '15s/^/#/' etc/nginx/sites-available/default
+if [ "$AUTOINDEX_USE" -eq 1 ];then
+	sed -i '21s/off/on/' etc/nginx/sites-available/default && \
+	sed -i '15s/^/#/' etc/nginx/sites-available/default
 fi
 service nginx start
 service mysql start
