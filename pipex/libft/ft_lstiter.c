9404da25f6b5a264ba324b21b4ed2429f73890e1
Taewan Kim <taewakim@c7r6s2.42seoul.kr>
2021-06-13 14:00:55 +0900

    update pipex makefile

---
+++ pipex/libft/ft_lstiter.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taewakim <taewakim@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/27 16:17:23 by taewakim          #+#    #+#             */
+/*   Updated: 2020/12/29 07:35:20 by taewakim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	while (lst)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
+}
